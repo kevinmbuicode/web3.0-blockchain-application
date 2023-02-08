8651940d2af24ad46dc04aa449500eb95a245729
kevinmbuicode <kevinmushan@gmail.com>
2023-02-08 17:38:22 +0300

    feat: add react icons, modify index.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navbar, Welcome, Footer, Services, Transactions , Loader} from './components';
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
 
 function App() {
   
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   );
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../../images/logo.png';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-full flex md:justify-center justify-between items-center p-4'>
       Navbar
     </div>
   )
