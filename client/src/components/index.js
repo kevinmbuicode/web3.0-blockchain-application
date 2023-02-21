757f87b19af3b41fd2a9272886efeb887b9bcca9
Kevin Mbui Mushan <77851451+kevinmbuicode@users.noreply.github.com>
2023-02-21 19:11:13 +0300

    fix: update services route on index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,5 @@ export { default as Loader } from './loader/Loader';
 export { default as Footer } from './footer/Footer';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Welcome } from './welcome/Welcome';
-export { default as Services } from './Services/Services';
+export { default as Services } from './services/Services';
 export { default as Transactions } from './transactions/Transactions';
